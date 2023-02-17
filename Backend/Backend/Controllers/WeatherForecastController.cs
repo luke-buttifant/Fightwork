d2aaee68a341075d69973539b32949021d77886f
luke-buttifant <lukerhys.lb@gmail.com>
2023-02-17 23:04:41 +0000

    proxied client to server

--- Backend/Backend/Controllers/WeatherForecastController.cs
+++ Backend/Backend/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace Backend.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            Console.Out.Write("HIT");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
                 {
